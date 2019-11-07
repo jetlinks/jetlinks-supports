32e57e2d5c98e35e0ab0839aa0e6e802a55eb096
zhou-hao <admin@hsweb.me>
2019-11-07 20:31:36 +0800

    增加配置元数据。

--- src/main/java/org/jetlinks/supports/official/JetLinksProtocolSupportProvider.java
+++ src/main/java/org/jetlinks/supports/official/JetLinksProtocolSupportProvider.java
@@ -2,6 +2,10 @@ package org.jetlinks.supports.official;
 
 import org.jetlinks.core.message.codec.DefaultTransport;
 import org.jetlinks.core.message.codec.DeviceMessageCodec;
+import org.jetlinks.core.metadata.ConfigMetadata;
+import org.jetlinks.core.metadata.DefaultConfigMetadata;
+import org.jetlinks.core.metadata.types.PasswordType;
+import org.jetlinks.core.metadata.types.StringType;
 import org.jetlinks.core.spi.ProtocolSupportProvider;
 import org.jetlinks.core.spi.ServiceContext;
 import org.jetlinks.core.defaults.CompositeProtocolSupport;
@@ -10,6 +14,16 @@ import reactor.core.publisher.Mono;
 import java.util.function.Supplier;
 
 public class JetLinksProtocolSupportProvider implements ProtocolSupportProvider {
+
+    private static final DefaultConfigMetadata mqttConfig = new DefaultConfigMetadata(
+            "MQTT认证配置"
+            , "MQTT认证时需要的配置,mqtt用户名,密码算法:\n" +
+            "username=secureId|timestamp\n" +
+            "password=md5(secureId|timestamp|secureKey)\n" +
+            "\n" +
+            "timestamp为时间戳,与服务时间不能相差5分钟")
+            .add("secureId", "secureId", "密钥ID", new StringType())
+            .add("secureKey", "secureKey", "密钥KEY", new PasswordType());
 
     @Override
     public Mono<CompositeProtocolSupport> create(ServiceContext context) {
@@ -23,7 +37,13 @@ public class JetLinksProtocolSupportProvider implements ProtocolSupportProvider 
 
             support.addAuthenticator(DefaultTransport.MQTT, new JetLinksAuthenticator());
             support.addAuthenticator(DefaultTransport.MQTTS, new JetLinksAuthenticator());
+
             support.setMetadataCodec(new JetLinksDeviceMetadataCodec());
+            Supplier<Mono<ConfigMetadata>> mqttConfigSupplier = () -> Mono.just(mqttConfig);
+
+            support.addConfigMetadata(DefaultTransport.MQTT, mqttConfigSupplier);
+            support.addConfigMetadata(DefaultTransport.MQTTS, mqttConfigSupplier);
+
             JetLinksMQTTDeviceMessageCodec codec = new JetLinksMQTTDeviceMessageCodec();
 
             Supplier<Mono<DeviceMessageCodec>> codecSupplier = () -> Mono.just(codec);
