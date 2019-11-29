4f8430ef1fce65d4f2694bcf1614daf6d52a7178
zhou-hao <admin@hsweb.me>
2019-11-29 16:17:30 +0800

    优化

--- src/main/java/org/jetlinks/supports/protocol/script/ScriptEvaluator.java
+++
@@ -1,8 +0,0 @@
-package org.jetlinks.supports.protocol.script;
-
-import java.util.Map;
-
-public interface ScriptEvaluator {
-    Object evaluate(String lang, String script, Map<String, Object> context) throws Exception;
-
-}
