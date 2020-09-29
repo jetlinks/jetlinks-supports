0ab50ec23d3ffd7f56a2a97afa05e385569ec656
zhou-hao <admin@hsweb.me>
2020-09-29 19:46:47 +0800

    优化

--- src/main/java/org/jetlinks/supports/utils/MqttTopicUtils.java
+++ src/main/java/org/jetlinks/supports/utils/MqttTopicUtils.java
@@ -1,10 +1,7 @@
 package org.jetlinks.supports.utils;
 
 import org.jetlinks.core.utils.TopicUtils;
-import org.springframework.util.AntPathMatcher;
-import org.springframework.util.PathMatcher;
 
-import java.util.HashMap;
 import java.util.Map;
 
 
@@ -13,28 +10,13 @@ import java.util.Map;
  */
 @Deprecated
 public class MqttTopicUtils {
-    private final static PathMatcher pathMatcher = new AntPathMatcher();
-
     public static boolean match(String topic, String target) {
-
-        if (topic.equals(target)) {
-            return true;
-        }
-
-        if (!topic.contains("#") && !topic.contains("+") && !topic.contains("{")) {
-            return false;
-        }
-
-        return pathMatcher.match(topic.replace("#", "**").replace("+", "*"), target);
+        return TopicUtils.match(topic, target);
 
     }
 
     public static Map<String, String> getPathVariables(String template, String topic) {
-        try {
-            return pathMatcher.extractUriTemplateVariables(template, topic);
-        } catch (Exception e) {
-            return new HashMap<>();
-        }
+        return TopicUtils.getPathVariables(template, topic);
     }
 
 }
