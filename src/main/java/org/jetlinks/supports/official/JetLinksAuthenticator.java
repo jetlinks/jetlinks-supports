0222704ede7eb6b28bf17f7bee09408e641789c3
zhou-hao <admin@hsweb.me>
2020-11-05 18:22:01 +0800

    优化scope

--- src/main/java/org/jetlinks/supports/official/JetLinksAuthenticator.java
+++ src/main/java/org/jetlinks/supports/official/JetLinksAuthenticator.java
@@ -2,10 +2,7 @@ package org.jetlinks.supports.official;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.jetlinks.core.Value;
-import org.jetlinks.core.device.AuthenticationRequest;
-import org.jetlinks.core.device.AuthenticationResponse;
-import org.jetlinks.core.device.DeviceOperator;
-import org.jetlinks.core.device.MqttAuthenticationRequest;
+import org.jetlinks.core.device.*;
 import org.jetlinks.core.defaults.Authenticator;
 import reactor.core.publisher.Mono;
 
@@ -14,6 +11,15 @@ import java.util.concurrent.TimeUnit;
 
 @Deprecated // https://github.com/jetlinks/jetlinks-official-protocol
 public class JetLinksAuthenticator implements Authenticator {
+
+    @Override
+    public Mono<AuthenticationResponse> authenticate(@Nonnull AuthenticationRequest request, @Nonnull DeviceRegistry registry) {
+        MqttAuthenticationRequest mqtt = ((MqttAuthenticationRequest) request);
+
+        return registry
+                .getDevice(mqtt.getClientId())
+                .flatMap(device -> authenticate(request, device));
+    }
 
     @Override
     public Mono<AuthenticationResponse> authenticate(@Nonnull AuthenticationRequest request, @Nonnull DeviceOperator deviceOperation) {
@@ -35,11 +41,15 @@ public class JetLinksAuthenticator implements Authenticator {
                 if (Math.abs(System.currentTimeMillis() - time) > TimeUnit.MINUTES.toMillis(5)) {
                     return Mono.just(AuthenticationResponse.error(401, "设备时间不同步"));
                 }
-                return deviceOperation.getConfigs("secureId", "secureKey")
+                return deviceOperation
+                        .getConfigs("secureId", "secureKey")
                         .map(conf -> {
-                            String secureId =  conf.getValue("secureId").map(Value::asString).orElse(null);
+                            String secureId = conf.getValue("secureId").map(Value::asString).orElse(null);
 
-                            String secureKey = conf.getValue("secureKey").map(Value::asString).orElse(null);
+                            String secureKey = conf
+                                    .getValue("secureKey")
+                                    .map(Value::asString)
+                                    .orElse(null);
                             //签名
                             String digest = DigestUtils.md5Hex(username + "|" + secureKey);
                             if (requestSecureId.equals(secureId) && digest.equals(password)) {
