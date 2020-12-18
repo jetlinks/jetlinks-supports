db71612b787f7ffa6bd56aff4f66e11c5f83b136
zhou-hao <admin@hsweb.me>
2020-12-18 20:37:42 +0800

    doOnReBalance

--- src/main/java/org/jetlinks/supports/cluster/redis/RedisHaManager.java
+++ src/main/java/org/jetlinks/supports/cluster/redis/RedisHaManager.java
@@ -7,6 +7,7 @@ import org.jetlinks.core.cluster.HaManager;
 import org.jetlinks.core.cluster.ServerNode;
 import org.springframework.data.redis.core.ReactiveHashOperations;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
+import reactor.core.Disposable;
 import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxProcessor;
@@ -19,6 +20,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -67,7 +69,7 @@ public class RedisHaManager implements HaManager {
         current.setLastKeepAlive(System.currentTimeMillis());
 
         inRedisNode.put(allNodeHashKey, current.getId(), current)
-                .subscribe();
+                   .subscribe();
 
         keepalive.publish(Mono.just(current)).subscribe();
 
@@ -79,31 +81,31 @@ public class RedisHaManager implements HaManager {
 
         //检查节点是否离线
         inRedisNode.keys(allNodeHashKey)
-                .filter(maybeOffline::containsKey)
-                .map(maybeOffline::get)
-                .collectList()
-                .filter(list -> !list.isEmpty())
-                .flatMapMany(list -> inRedisNode
-                        .remove(allNodeHashKey, list.stream().map(ServerNode::getId).toArray())
-                        .thenMany(Flux.fromIterable(list))
-                )
-                .as(offlineTopic::publish)
-                .subscribe();
+                   .filter(maybeOffline::containsKey)
+                   .map(maybeOffline::get)
+                   .collectList()
+                   .filter(list -> !list.isEmpty())
+                   .flatMapMany(list -> inRedisNode
+                           .remove(allNodeHashKey, list.stream().map(ServerNode::getId).toArray())
+                           .thenMany(Flux.fromIterable(list))
+                   )
+                   .as(offlineTopic::publish)
+                   .subscribe();
     }
 
     private void electionLeader() {
         allNode.values()
-                .stream()
-                .peek(serverNode -> serverNode.setLeader(false))
-                .min(Comparator.comparing(ServerNode::getUptime))
-                .ifPresent(serverNode -> serverNode.setLeader(true));
+               .stream()
+               .peek(serverNode -> serverNode.setLeader(false))
+               .min(Comparator.comparing(ServerNode::getUptime))
+               .ifPresent(serverNode -> serverNode.setLeader(true));
     }
 
     public void shutdown() {
         inRedisNode
                 .remove(allNodeHashKey, current.getId())
                 .then(offlineTopic
-                        .publish(Mono.just(current)))
+                              .publish(Mono.just(current)))
                 .block();
     }
 
@@ -116,64 +118,64 @@ public class RedisHaManager implements HaManager {
 
         //注册自己
         inRedisNode.put(allNodeHashKey, current.getId(), current)
-                .flatMapMany(r -> inRedisNode.values(allNodeHashKey))
-                .collectList()
-                .doOnNext(node -> {
-                    for (ServerNode serverNode : node) {
-                        serverNode.setLastKeepAlive(System.currentTimeMillis());
-                        allNode.put(serverNode.getId(), serverNode);
-                    }
-                    electionLeader();
-                    Flux.interval(Duration.ZERO, Duration.ofSeconds(5))
-                            .doOnNext(i -> this.checkAlive())
-                            .subscribe();
-                })
-                .block();
+                   .flatMapMany(r -> inRedisNode.values(allNodeHashKey))
+                   .collectList()
+                   .doOnNext(node -> {
+                       for (ServerNode serverNode : node) {
+                           serverNode.setLastKeepAlive(System.currentTimeMillis());
+                           allNode.put(serverNode.getId(), serverNode);
+                       }
+                       electionLeader();
+                       Flux.interval(Duration.ZERO, Duration.ofSeconds(5))
+                           .doOnNext(i -> this.checkAlive())
+                           .subscribe();
+                   })
+                   .block();
 
         offlineTopic.subscribe()
-                .subscribe(serverNode -> {
-                    //自己
-                    if (currentServer().isSame(serverNode)) {
-                        return;
-                    }
-                    if (allNode.remove(serverNode.getId()) != null) {
-                        log.debug("[{}]:server node [{}] offline", haName, serverNode.getId());
-                        //node offline
-                        inRedisNode
-                                .remove(allNodeHashKey, serverNode.getId())
-                                .subscribe();
-                        electionLeader();
-                        if (offlineProcessor.hasDownstreams()) {
-                            offlineProcessor.onNext(serverNode);
+                    .subscribe(serverNode -> {
+                        //自己
+                        if (currentServer().isSame(serverNode)) {
+                            return;
                         }
-                    }
-                });
+                        if (allNode.remove(serverNode.getId()) != null) {
+                            log.debug("[{}]:server node [{}] offline", haName, serverNode.getId());
+                            //node offline
+                            inRedisNode
+                                    .remove(allNodeHashKey, serverNode.getId())
+                                    .subscribe();
+                            electionLeader();
+                            if (offlineProcessor.hasDownstreams()) {
+                                offlineProcessor.onNext(serverNode);
+                            }
+                        }
+                    });
         //其他节点定时发布
         keepalive.subscribe()
-                .subscribe(serverNode -> {
-                    //自己
-                    if (currentServer().isSame(serverNode)) {
-                        return;
-                    }
-                    serverNode.setLastKeepAlive(System.currentTimeMillis());
-                    allNode.compute(serverNode.getId(), (id, node) -> {
-                        if (node != null) {
-                            node.setLastKeepAlive(System.currentTimeMillis());
-                            return node;
-                        }
-                        return null;
-                    });
-                    if (!allNode.containsKey(serverNode.getId())) {
-                        allNode.put(serverNode.getId(), serverNode);
-                        electionLeader();
-                        log.debug("[{}]:server node [{}] online", haName, serverNode.getId());
-                        //node join
-                        if (onlineProcessor.hasDownstreams()) {
-                            onlineProcessor.onNext(serverNode);
-                        }
-                    }
+                 .subscribe(serverNode -> {
+                     //自己
+                     if (currentServer().isSame(serverNode)) {
+                         return;
+                     }
+                     serverNode.setLastKeepAlive(System.currentTimeMillis());
+                     allNode.compute(serverNode.getId(), (id, node) -> {
+                         if (node != null) {
+                             node.setLastKeepAlive(System.currentTimeMillis());
+                             return node;
+                         }
+                         return null;
+                     });
+                     if (!allNode.containsKey(serverNode.getId())) {
+                         allNode.put(serverNode.getId(), serverNode);
+                         electionLeader();
+                         log.debug("[{}]:server node [{}] online", haName, serverNode.getId());
+                         //node join
+                         if (onlineProcessor.hasDownstreams()) {
+                             onlineProcessor.onNext(serverNode);
+                         }
+                     }
 
-                });
+                 });
 
 
     }
@@ -198,5 +200,11 @@ public class RedisHaManager implements HaManager {
     @Override
     public List<ServerNode> getAllNode() {
         return new ArrayList<>(allNode.values());
+    }
+
+    @Override
+    public Disposable doOnReBalance(Consumer<List<ServerNode>> runnable) {
+        return () -> {
+        };
     }
 }
