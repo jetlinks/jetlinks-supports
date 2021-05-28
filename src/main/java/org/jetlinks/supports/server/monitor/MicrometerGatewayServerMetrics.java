ca8735efc3d6b1d4f22197be68bf3b9644ef41dd
zhou-hao <admin@hsweb.me>
2021-05-28 09:07:21 +0800

    优化会话统计

--- src/main/java/org/jetlinks/supports/server/monitor/MicrometerGatewayServerMetrics.java
+++ src/main/java/org/jetlinks/supports/server/monitor/MicrometerGatewayServerMetrics.java
@@ -6,11 +6,17 @@ import io.micrometer.core.instrument.Metrics;
 import org.jetlinks.core.server.monitor.GatewayServerMetrics;
 import org.jetlinks.core.server.session.DeviceSession;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class MicrometerGatewayServerMetrics implements GatewayServerMetrics {
 
-    private String serverId;
+    private final String serverId;
 
-    private MeterRegistry registry;
+    private final MeterRegistry registry;
+
+    private final Map<String, AtomicLong> sessionRecord = new ConcurrentHashMap<>();
 
     public MicrometerGatewayServerMetrics(String serverId) {
         this(serverId, null);
@@ -23,6 +29,14 @@ public class MicrometerGatewayServerMetrics implements GatewayServerMetrics {
             registry = Metrics.globalRegistry;
         }
         this.registry = registry;
+
+        Gauge.builder("gateway-server-session", sessionRecord, sessionRecord -> sessionRecord
+                .values()
+                .stream()
+                .mapToLong(Number::longValue)
+                .sum())
+             .tag("server", getServerId())
+             .register(registry);
     }
 
     public String getServerId() {
@@ -31,11 +45,9 @@ public class MicrometerGatewayServerMetrics implements GatewayServerMetrics {
 
     @Override
     public void reportSession(String transport, int sessionTotal) {
-        Gauge.builder("gateway-server-session", sessionTotal, Number::doubleValue)
-                .tag("transport", transport)
-                .tag("server", getServerId())
-                .description("当前" + transport + "会话数量")
-                .register(registry);
+        sessionRecord
+                .computeIfAbsent(transport, key -> new AtomicLong())
+                .set(sessionTotal);
     }
 
     @Override
@@ -65,9 +77,9 @@ public class MicrometerGatewayServerMetrics implements GatewayServerMetrics {
     @Override
     public void receiveFromDeviceMessage(DeviceSession session) {
         registry.counter("received_messages",
-                "transport", session.getTransport().getId(),
-                "deviceId", session.getDeviceId(),
-                "server", getServerId())
-                .increment(-1);
+                         "transport", session.getTransport().getId(),
+                         "deviceId", session.getDeviceId(),
+                         "server", getServerId())
+                .increment(1);
     }
 }
