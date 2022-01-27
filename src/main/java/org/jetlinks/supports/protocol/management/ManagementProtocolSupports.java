80230faff6fe5c553cd7b965553972c35e406008
zhouhao <zh.sqy@qq.com>
2022-01-27 15:52:27 +0800

    优化说明

--- src/main/java/org/jetlinks/supports/protocol/management/ManagementProtocolSupports.java
+++ src/main/java/org/jetlinks/supports/protocol/management/ManagementProtocolSupports.java
@@ -10,6 +10,12 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 
+/**
+ * 动态管理协议包支持. 实现集群时协议包动态管理.
+ *
+ * @author zhouhao
+ * @since 1.0
+ */
 @Slf4j
 @Setter
 public class ManagementProtocolSupports extends StaticProtocolSupports {
@@ -22,17 +28,31 @@ public class ManagementProtocolSupports extends StaticProtocolSupports {
 
     private Map<String, String> configProtocolIdMapping = new ConcurrentHashMap<>();
 
-
+    /**
+     * 初始化
+     *
+     * <pre>
+     *     1.加载全部协议
+     *     2.订阅topic,来监听集群的协议操作通知
+     * </pre>
+     */
     public void init() {
         manager.loadAll()
-                .filter(de -> de.getState() == 1)
-                .subscribe(this::init);
+               .filter(de -> de.getState() == 1)
+               .subscribe(this::init);
 
         clusterManager.<ProtocolSupportDefinition>getTopic("_protocol_changed")
-                .subscribe()
-                .subscribe(this::init);
+                      .subscribe()
+                      .subscribe(this::init);
     }
 
+    /**
+     * 初始化协议
+     * <p>
+     * 如果{@link ProtocolSupportDefinition#getState()}不为1，则表示卸载协议。为0则表示加载协议.
+     *
+     * @param definition 协议定义
+     */
     public void init(ProtocolSupportDefinition definition) {
         if (definition.getState() != 1) {
             String protocol = configProtocolIdMapping.get(definition.getId());
@@ -48,11 +68,11 @@ public class ManagementProtocolSupports extends StaticProtocolSupports {
         log.debug("{} protocol:{}", operation, definition);
         try {
             loader.load(definition)
-                    .doOnNext(protocolSupport -> protocolSupport.init(definition.getConfiguration()))
-                    .doOnError(e -> log.error("{} protocol[{}] error: {}", operation, definition.getId(), e))
-                    .doOnNext(e -> log.debug("{} protocol[{}] success: {}", operation, definition.getId(), e))
-                    .doOnNext(e -> configProtocolIdMapping.put(definition.getId(), e.getId()))
-                    .subscribe(consumer);
+                  .doOnNext(protocolSupport -> protocolSupport.init(definition.getConfiguration()))
+                  .doOnError(e -> log.error("{} protocol[{}] error: {}", operation, definition.getId(), e))
+                  .doOnNext(e -> log.debug("{} protocol[{}] success: {}", operation, definition.getId(), e))
+                  .doOnNext(e -> configProtocolIdMapping.put(definition.getId(), e.getId()))
+                  .subscribe(consumer);
         } catch (Exception e) {
             log.error("load protocol error:{}", definition, e);
         }
--- src/main/java/org/jetlinks/supports/protocol/management/jar/JarProtocolSupportLoader.java
+++ src/main/java/org/jetlinks/supports/protocol/management/jar/JarProtocolSupportLoader.java
@@ -18,14 +18,32 @@ import java.util.Optional;
 import java.util.ServiceLoader;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * 动态加载Jar包来实现协议支持.
+ * jar包不支持使用第三方依赖.
+ * 如果需要依赖第三方包，可以使用maven-shade-plugin方式，将依赖class和协议打包在一起。
+ * <p>
+ * 配置格式:<pre>{@code
+ * {
+ *    "location":"jar文件地址",
+ *    "provider":"ProtocolSupportProvider实现类全名"
+ * }
+ * }</pre>
+ *
+ * @author zhouhao
+ * @see ProtocolSupportProvider
+ * @since 1.0
+ */
 @Slf4j
 public class JarProtocolSupportLoader implements ProtocolSupportLoaderProvider {
 
     @Setter
     private ServiceContext serviceContext;
 
+    //ClassLoader缓存
     private final Map<String, ProtocolClassLoader> protocolLoaders = new ConcurrentHashMap<>();
 
+    //已加载的协议提供商
     private final Map<String, ProtocolSupportProvider> loaded = new ConcurrentHashMap<>();
 
     @Override
@@ -33,20 +51,6 @@ public class JarProtocolSupportLoader implements ProtocolSupportLoaderProvider {
         return "jar";
     }
 
-    protected ProtocolClassLoader createClassLoader(URL location) {
-        return new ProtocolClassLoader(new URL[]{location}, this.getClass().getClassLoader());
-    }
-
-    protected void closeAll() {
-        protocolLoaders.values().forEach(this::closeLoader);
-        protocolLoaders.clear();
-        loaded.clear();
-    }
-
-    @SneakyThrows
-    protected void closeLoader(ProtocolClassLoader loader) {
-        loader.close();
-    }
 
     @Override
     @SneakyThrows
@@ -54,25 +58,29 @@ public class JarProtocolSupportLoader implements ProtocolSupportLoaderProvider {
         return Mono.defer(() -> {
             try {
                 Map<String, Object> config = definition.getConfiguration();
+                //文件地址
                 String location = Optional
                         .ofNullable(config.get("location"))
                         .map(String::valueOf)
                         .orElseThrow(() -> new IllegalArgumentException("location"));
-
+                //ProtocolSupportProvider类全名
                 String provider = Optional.ofNullable(config.get("provider"))
                                           .map(String::valueOf)
                                           .map(String::trim).orElse(null);
                 URL url;
 
                 if (!location.contains("://")) {
+                    //本地文件
                     url = new File(location).toURI().toURL();
                 } else {
+                    //url
                     url = new URL("jar:" + location + "!/");
                 }
 
                 ProtocolClassLoader loader;
                 URL fLocation = url;
                 {
+                    //释放旧的provider
                     ProtocolSupportProvider oldProvider = loaded.remove(provider);
                     if (null != oldProvider) {
                         oldProvider.dispose();
@@ -81,6 +89,7 @@ public class JarProtocolSupportLoader implements ProtocolSupportLoaderProvider {
                 loader = protocolLoaders.compute(definition.getId(), (key, old) -> {
                     if (null != old) {
                         try {
+                            //释放旧的ClassLoader
                             closeLoader(old);
                         } catch (Exception ignore) {
                         }
@@ -96,20 +105,54 @@ public class JarProtocolSupportLoader implements ProtocolSupportLoaderProvider {
                     Class<ProtocolSupportProvider> providerType = (Class) loader.loadSelfClass(provider);
                     supportProvider = providerType.getDeclaredConstructor().newInstance();
                 } else {
+                    //没有指定provider，则通过SPI来获取
                     supportProvider = ServiceLoader.load(ProtocolSupportProvider.class, loader).iterator().next();
                 }
+
                 ProtocolSupportProvider oldProvider = loaded.put(provider, supportProvider);
                 try {
+                    //如果存在旧的则释放掉
                     if (null != oldProvider) {
                         oldProvider.dispose();
                     }
                 } catch (Exception e) {
                     log.error(e.getMessage(), e);
                 }
+                //创建协议支持
                 return supportProvider.create(serviceContext);
             } catch (Throwable e) {
                 return Mono.error(e);
             }
         }).subscribeOn(Schedulers.elastic());
+    }
+
+    /**
+     * 根据URL创建ClassLoader
+     *
+     * @param location location
+     * @return ProtocolClassLoader
+     */
+    protected ProtocolClassLoader createClassLoader(URL location) {
+        return new ProtocolClassLoader(new URL[]{location}, this.getClass().getClassLoader());
+    }
+
+    /**
+     * 释放全部信息
+     */
+    protected void closeAll() {
+        protocolLoaders.values().forEach(this::closeLoader);
+        protocolLoaders.clear();
+        loaded.values().forEach(ProtocolSupportProvider::dispose);
+        loaded.clear();
+    }
+
+    /**
+     * 关闭(释放)classLoader
+     *
+     * @param loader classLoader
+     */
+    @SneakyThrows
+    protected void closeLoader(ProtocolClassLoader loader) {
+        loader.close();
     }
 }
