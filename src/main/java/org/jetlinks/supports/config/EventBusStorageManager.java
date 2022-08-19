b10322bcac9277a662fe536b71ab920564c251fd
zhouhao <zh.sqy@qq.com>
2022-08-19 13:56:20 +0800

    优化集群通知逻辑

--- src/main/java/org/jetlinks/supports/config/EventBusStorageManager.java
+++ src/main/java/org/jetlinks/supports/config/EventBusStorageManager.java
@@ -10,18 +10,27 @@ import org.jetlinks.core.config.ConfigStorage;
 import org.jetlinks.core.config.ConfigStorageManager;
 import org.jetlinks.core.event.EventBus;
 import org.jetlinks.core.event.Subscription;
+import reactor.core.Disposable;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Slf4j
-public class EventBusStorageManager implements ConfigStorageManager {
+public class EventBusStorageManager implements ConfigStorageManager, Disposable {
 
+    private static final AtomicReferenceFieldUpdater<EventBusStorageManager, Disposable>
+            CLUSTER_SUBSCRIBER = AtomicReferenceFieldUpdater.newUpdater(
+            EventBusStorageManager.class, Disposable.class, "disposable"
+    );
     final ConcurrentMap<String, LocalCacheClusterConfigStorage> cache;
 
+    private volatile Disposable disposable;
+
+    private final EventBus eventBus;
     private final Function<String, LocalCacheClusterConfigStorage> storageBuilder;
 
     @Deprecated
@@ -51,6 +60,7 @@ public class EventBusStorageManager implements ConfigStorageManager {
                                   EventBus eventBus,
                                   long ttl) {
         this.cache = new NonBlockingHashMap<>();
+        this.eventBus = eventBus;
         storageBuilder = id -> {
             return new LocalCacheClusterConfigStorage(
                     id,
@@ -61,7 +71,10 @@ public class EventBusStorageManager implements ConfigStorageManager {
                         this.cache.remove(id);
                     });
         };
-        eventBus
+    }
+
+    private Disposable subscribeCluster() {
+        return eventBus
                 .subscribe(Subscription
                                    .builder()
                                    .subscriberId("event-bus-storage-listener")
@@ -87,8 +100,23 @@ public class EventBusStorageManager implements ConfigStorageManager {
     }
 
     @Override
+    public void dispose() {
+        if (disposable != null) {
+            disposable.dispose();
+        }
+    }
+
+    @Override
     @SneakyThrows
     public Mono<ConfigStorage> getStorage(String id) {
+        if (disposable == null) {
+            synchronized (this){
+                Disposable disp = subscribeCluster();
+                if (!CLUSTER_SUBSCRIBER.compareAndSet(this, null, disp)) {
+                    disp.dispose();
+                }
+            }
+        }
         return Mono.fromSupplier(() -> cache.computeIfAbsent(id, storageBuilder));
     }
 }
