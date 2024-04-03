ea4c58ce26ffa4b37bb75406c0d58adddc416851
zhouhao <zh.sqy@qq.com>
2024-04-03 14:18:16 +0800

    refactor: 优化

--- src/main/java/org/jetlinks/supports/official/types/JetLinksEnumCodec.java
+++ src/main/java/org/jetlinks/supports/official/types/JetLinksEnumCodec.java
@@ -3,7 +3,10 @@ package org.jetlinks.supports.official.types;
 import com.alibaba.fastjson.JSONObject;
 import lombok.Getter;
 import lombok.Setter;
+import org.jetlinks.core.metadata.DataType;
+import org.jetlinks.core.metadata.types.DataTypes;
 import org.jetlinks.core.metadata.types.EnumType;
+import org.jetlinks.supports.official.JetLinksDataTypeCodecs;
 
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -21,15 +24,36 @@ public class JetLinksEnumCodec extends AbstractDataTypeCodec<EnumType> {
 
     @Override
     public EnumType decode(EnumType type, Map<String, Object> config) {
-        super.decode(type,config);
+        super.decode(type, config);
         JSONObject jsonObject = new JSONObject(config);
 
         ofNullable(jsonObject.getJSONArray("elements"))
-                .map(list -> list.stream()
-                        .map(JSONObject.class::cast)
-                        .map(e -> EnumType.Element.of(e.getString("value"), e.getString("text"),e.getString("description")))
-                        .collect(Collectors.toList()))
-                .ifPresent(type::setElements);
+            .map(list -> list
+                .stream()
+                .map(JSONObject.class::cast)
+                .map(e -> EnumType.Element.of(e.getString("value"), e.getString("text"), e.getString("description")))
+                .collect(Collectors.toList()))
+            .ifPresent(type::setElements);
+
+        ofNullable(jsonObject.get("valueType"))
+            .map(v -> {
+                if (v instanceof Map) {
+                    return new JSONObject(((Map<String, Object>) v));
+                }
+                // {valueType:int}
+                JSONObject eleType = new JSONObject();
+                eleType.put("type", v);
+                return eleType;
+            })
+            .map(eleType -> {
+                DataType dataType = DataTypes.lookup(eleType.getString("type")).get();
+
+                JetLinksDataTypeCodecs.getCodec(dataType.getId())
+                                      .ifPresent(codec -> codec.decode(dataType, eleType));
+
+                return dataType;
+            })
+            .ifPresent(type::setValueType);
 
         return type;
     }
@@ -37,12 +61,15 @@ public class JetLinksEnumCodec extends AbstractDataTypeCodec<EnumType> {
     @Override
     protected void doEncode(Map<String, Object> encoded, EnumType type) {
         super.doEncode(encoded, type);
-        if(type.getElements()==null){
+        if (type.getElements() == null) {
             return;
         }
-        encoded.put("elements", type.getElements()
-                .stream()
-                .map(EnumType.Element::toMap).collect(Collectors.toList()));
+        encoded.put("elements", type
+            .getElements()
+            .stream()
+            .map(EnumType.Element::toMap).collect(Collectors.toList()));
+
+        encoded.put("valueType", JetLinksDataTypeCodecs.encode(type.getValueType()).orElse(null));
 
     }
 }
