0ea6ef5275089f862abb7c26cae72ccafb36a5c5
zhouhao <zh.sqy@qq.com>
2024-04-07 15:47:39 +0800

    feat: 增加求2个设备物模型差集的工具类

--- src/main/java/org/jetlinks/supports/utils/DeviceMetadataUtils.java
+++ src/main/java/org/jetlinks/supports/utils/DeviceMetadataUtils.java
@@ -1,7 +1,6 @@
 package org.jetlinks.supports.utils;
 
-import org.jetlinks.core.metadata.PropertyMetadata;
-import org.jetlinks.core.metadata.SimplePropertyMetadata;
+import org.jetlinks.core.metadata.*;
 import org.jetlinks.supports.official.DeviceMetadataParser;
 import org.springframework.core.ResolvableType;
 
@@ -9,9 +8,21 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * 设备物模型工具类
+ *
+ * @author zhouhao
+ * @since 1.2
+ */
 public class DeviceMetadataUtils {
 
 
+    /**
+     * 根据一个Map作为数据模版转为物模型属性信息
+     *
+     * @param value map 数据模版
+     * @return 属性信息
+     */
     public static List<PropertyMetadata> convertToProperties(Map<String, Object> value) {
         return value
             .entrySet()
@@ -27,6 +38,54 @@ public class DeviceMetadataUtils {
                 return metadata;
             })
             .collect(Collectors.toList());
+    }
+
+
+    /**
+     * 获取left中定义了但是right中没有定义的物模型.
+     * <pre>{@code
+     *
+     *    new CompositeDeviceMetadata(left,right).difference();
+     *
+     * }</pre>
+     *
+     * @return DeviceMetadata
+     * @param left 基准物模型
+     * @param right 对比物模型
+     * @since 1.2.2
+     */
+    public static DeviceMetadata difference(DeviceMetadata left, DeviceMetadata right) {
+        SimpleDeviceMetadata metadata = new SimpleDeviceMetadata();
+        metadata.setId(right.getId());
+        metadata.setName(right.getName());
+        metadata.setExpands(right.getExpands());
+        metadata.setDescription(right.getDescription());
+
+        for (PropertyMetadata meta : right.getProperties()) {
+            if (left.getPropertyOrNull(meta.getId()) == null) {
+                metadata.addProperty(meta);
+            }
+        }
+
+        for (EventMetadata meta : right.getEvents()) {
+            if (left.getEventOrNull(meta.getId()) == null) {
+                metadata.addEvent(meta);
+            }
+        }
+
+        for (FunctionMetadata meta : right.getFunctions()) {
+            if (left.getFunctionOrNull(meta.getId()) == null) {
+                metadata.addFunction(meta);
+            }
+        }
+
+        for (PropertyMetadata meta : right.getTags()) {
+            if (left.getTagOrNull(meta.getId()) == null) {
+                metadata.addTag(meta);
+            }
+        }
+
+        return metadata;
     }
 
 }
